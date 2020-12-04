3444c15f1332eac7a7c74b805fbb1bf813283bd6
Darwin Salinas <salinash2000@gmail.com>
2020-12-03 20:49:34 -0600

    clean default todo

--- src/store/atoms.js
+++ src/store/atoms.js
@@ -2,7 +2,7 @@ import { atom } from "recoil";
 
 export const draftTodoState = atom({
   key: "draftTodoState",
-  default: "mi todo por defecto",
+  default: "",
 });
 
 export const todosState = atom({
