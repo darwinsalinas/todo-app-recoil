8f75031b1de7d36fdb80da795dae750ef98cbd2f
Darwin Salinas <salinash2000@gmail.com>
2020-12-03 21:04:26 -0600

    estado inicial de la lista para recordar que hacer en la app

--- src/store/atoms.js
+++ src/store/atoms.js
@@ -8,6 +8,13 @@ export const draftTodoState = atom({
 export const todosState = atom({
   key: "todosState",
   default: [
-
+    {
+      id: 1,
+      todo: 'Ponerle los estilos a la lista de elementos'
+    },
+    {
+      id: 2,
+      todo: 'Usar Local Storage para persistir los datos'
+    }
   ],
 });
