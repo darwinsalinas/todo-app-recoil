c0a615a7d49a23ff8061b4fd0d82201c313cb24f
Darwin Salinas <salinash2000@gmail.com>
2020-12-04 22:42:08 -0600

    todos para la app

--- src/store/atoms.js
+++ src/store/atoms.js
@@ -10,11 +10,35 @@ export const todosState = atom({
   default: [
     {
       id: 1,
-      todo: 'Ponerle los estilos a la lista de elementos'
+      todo: 'Esta app usa RecoilJS para manejar el estado global'
     },
     {
       id: 2,
       todo: 'Usar Local Storage para persistir los datos'
+    },
+    {
+      id: 3,
+      todo: 'Permitir al usuario alternar entre un tema oscuro y claro'
+    },
+    {
+      id: 4,
+      todo: 'Permitir al usuario marcar las tareas como completadas'
+    },
+    {
+      id: 5,
+      todo: 'Permitir al usuario borrar items de la lista'
+    },
+    {
+      id: 6,
+      todo: 'Permitir al usuario borrar todos los items completados de la lista'
+    },
+    {
+      id: 7,
+      todo: 'Permitir hacer filtro de las items completados, activos y todos los items'
+    },
+    {
+      id: 8,
+      todo: 'Mostrar al usuario la cantidad de items que hacen falta por completar'
     }
   ],
 });
